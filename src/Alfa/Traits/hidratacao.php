b8a4f0bd0884bb5afbd14a233da70d4f9670b121
Eduardo Kawassaki Ferreira - T.I Gazin <eduardo.kawassaki@gazin.com.br>
2017-07-06 14:41:31 -0300

    hidratacao

--- src/Alfa/Traits/hidratacao.php
+++ src/Alfa/Traits/hidratacao.php
@@ -4,14 +4,8 @@ trait Hidratacao
 {
     public function hidrata(array $dados)
     {
-        foreach ($dados as $col => $val) {
-            if ($val) 
-            {
-                continue;
-            }else
-            {
-                break;
-            }
+        foreach( $dados as $key => $val ) {
+                $this->$key = $val;
          }
     }
 }
